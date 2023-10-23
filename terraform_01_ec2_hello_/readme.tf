a92fee1934a0403a3d8a6e73ec58cd338516aa6e
codvatechlabs <78929919+codvatechlabs@users.noreply.github.com>
2023-10-23 18:16:17 +0530

    Create readme.tf

---
+++ terraform_01_ec2_hello_/readme.tf
@@ -0,0 +1,31 @@
+# Terraform - Setup EC2 using Terraform 
+Setup Terraform EC2 instance using Terraform 
+
+## Prerequisites
+
+Before getting started, make sure you have the following prerequisites installed:
+
+- [Terraform](https://www.terraform.io/downloads.html)
+- AWS CLI configured with appropriate credentials
+
+## Run below commands 
+
+```
+
+1. Initialize the Terraform configuration:
+
+   ```shell
+   terraform init
+   ```
+
+2. Plan the infrastructure:
+
+   ```shell
+   terraform plan
+   ```
+
+3. Deploy the infrastructure for the `dev` environment:
+
+   ```shell
+   terraform apply
+   ```
